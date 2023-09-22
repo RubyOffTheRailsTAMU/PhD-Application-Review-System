f510b9bb0d4142028439022af17af2d3f1a2a981
tanaypatankar <tanaypatankar.2000@gmail.com>
2023-09-22 14:39:26 -0500

    temp: undo user logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
         auth = request.env['omniauth.auth']
         session[:user_token] = auth.credentials.token
         session[:user_email] = auth.info.email
+        redirect_to root_path
+        # user = User.find_by(email: email)
 
-        user = User.find_by(email: email)
-
-        if user.nil?
-            redirect_to new_user_path, alert: 'Create an account before signing in with Google'
-        else
-            redirect_to root_path
-      end
+        # if user.nil?
+        #     redirect_to new_user_path, alert: 'Create an account before signing in with Google'
+        # else
+        #     redirect_to root_path
+        # end
     end
       def destroy
         reset_session
