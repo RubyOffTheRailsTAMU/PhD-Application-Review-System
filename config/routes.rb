e0f53767fdc11e6a37fd5be9efa47844ad446ba7
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-09-24 18:58:25 -0500

    Candidates Crud n stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :candidates
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get "/candidates", to: "candidates#index"
   root 'welcome#index'
+  
 end
