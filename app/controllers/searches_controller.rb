de7dc4b89693f879ee20555b0b6774bd2b465ccb
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-10-13 16:48:55 -0500

    Update searches_controller.rb

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
         query = params[:query]
         field = params[:field]
         #todo: non local
-        uri = URI("http://localhost:3001/api/v1/searches?query=#{params[:query]}&field=#{params[:field]}")
+        uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{params[:query]}&field=#{params[:field]}")
         response = Net::HTTP.get(uri)
         @results = JSON.parse(response)
       else
