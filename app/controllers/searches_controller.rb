29418c1c3815954cb0cd9befa44355da6ac7fcd7
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-10-22 19:05:22 -0500

    search index page integration 2.0 and applicant review page link

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,10 @@
 class SearchesController < ApplicationController
     require 'net/http'
     before_action :require_user
+
     def index
       if params[:query].present?
-        query = params[:query]
-        field = params[:field]
-        uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{params[:query]}&field=#{params[:field]}")
-        response = Net::HTTP.get(uri)
-        @results = JSON.parse(response)
+        @results = SearchService.search(query: params[:query], field: params[:field])
       else
         @results = []
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,24 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root 'welcome#index'
+
   resources :tasks
   resources :users
+  resources :reviews
+  resources :applicants
+
   get 'xlsx/tojson'
   get 'csv/tojson'
   get 'applicants/savedata'
-  resources :reviews
-  resources :users
-  resources :candidates
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :candidates
-  # Defines the root path route ("/")
-  # root "articles#index"
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get "/candidates", to: "candidates#index"
-  get "/users", to: "users#index"
-  get "/reviews", to: "reviews#index"
   get '/home', to: 'homepage#index'
   post '/login', to: 'welcome#create'
   get '/searches', to: 'searches#index'
   get '/admin', to: 'welcome#_admin'
+  get 'application', to: 'application#index'
 
-  root 'welcome#index'
 
-  post 'search', to: 'searches#search', as: 'search'
-
+  post 'searches', to: 'searches#index', as: 'search'
 end
