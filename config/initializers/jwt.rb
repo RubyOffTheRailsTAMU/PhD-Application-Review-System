4e9e22d0ab093fafb5eddc943971cf76fb6d98a7
XRFXRF <Felixxrf@outlook.com>
2023-10-29 21:04:57 -0500

    fix key bug

--- config/initializers/jwt.rb
+++ config/initializers/jwt.rb
@@ -3,4 +3,4 @@ require 'base64'
 
 JWT_ALGORITHM = 'RS256'
 # JWT_PRIVATE_KEY = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('private_key.pem')))
-JWT_PRIVATE_KEY = Base64.strict_decode64(ENV['PRIVATE_KEY'])
+JWT_PRIVATE_KEY = OpenSSL::PKey::RSA.new(Base64.strict_decode64(ENV['PRIVATE_KEY']))
--- test.rb
+++ test.rb
@@ -8,3 +8,15 @@ puts "qwe: " + encoded_private_key
 encoded_public_key = Base64.strict_encode64('public_key.pem')
 
 puts "rtrt: "+encoded_public_key
+
+
+jwt_public_key = Base64.strict_decode64(encoded_public_key)
+jwt_private_key = Base64.strict_decode64(encoded_private_key)
+
+puts "Private:"
+puts jwt_private_key
+puts "Public: "
+puts jwt_public_key
+
+puts "read"
+puts File.read('private_key.pem')
