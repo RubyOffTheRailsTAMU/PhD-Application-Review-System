3672d0a208daf41f00b676c3ef9e771cb9c91f03
XRFXRF <Felixxrf@outlook.com>
2023-10-29 20:41:23 -0500

    implement jwt on heroku

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -2,12 +2,12 @@ class SearchService
     require 'net/http'
   
     def self.search(query:, field: ,token:)
-        # uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{query}&field=#{field}")
-        uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
+        uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{query}&field=#{field}")
+        # uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
         http = Net::HTTP.new(uri.host, uri.port)
 
         request = Net::HTTP::Get.new(uri)
-        request['Authorization'] = "Bearer #{token}" # 添加 Authorization 头部
+        request['Authorization'] = "Bearer #{token}" 
 
         response = http.request(request)
         JSON.parse(response.body)
