c77dc3e195df555768062869724c90af599ab3e2
XRFXRF <Felixxrf@outlook.com>
2023-11-03 11:10:08 -0500

    add token for admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,10 @@ class WelcomeController < ApplicationController
         if user && params[:session][:user_name] == "admin" && user.authenticate(params[:session][:password])
             session[:user_token] = "admin_token_placeholder"
             session[:user_email] = user.user_email
+
+            jwtToken=JwtService.generate_jwt({user_email:session[:user_email]})
+            session[:jwt_token] = jwtToken
+            
             current_user
             redirect_to '/home'
           else
