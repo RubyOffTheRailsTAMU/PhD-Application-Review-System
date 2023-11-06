d4c52bc6ffbae32b3b5c3fb13552f29d3c96097e
Meetansh <guptameetansh@gmail.com>
2023-11-06 11:46:39 -0600

    no more redirect to some random page + logic error fix for new reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,7 +31,9 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+        # format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+        flash[:notice] = "Review was successfully updated."
+        format.html { redirect_to "/application?cas_id=#{@review.applicant_id}" }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -50,7 +52,9 @@ class ReviewsController < ApplicationController
       end
 
       if @review.update(review_params)
-        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+        # format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+        flash[:notice] = "Review was successfully updated."
+        format.html { redirect_to "/application?cas_id=#{@review.applicant_id}" }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit, status: :unprocessable_entity }
