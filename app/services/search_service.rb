0d4704242f650f0e1ba9e38a8a7d9625b9028f45
shreyasskasetty-tamu <142867885+shreyasskasetty-tamu@users.noreply.github.com>
2023-11-17 17:52:06 -0600

    change the search URL

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -2,8 +2,8 @@ class SearchService
     require 'net/http'
 
     def self.search(query:, field: ,token:)
-        # uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{query}&field=#{field}")
-        uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
+        uri = URI("https://phdapplicationsystemdatabase-5e5fc255409c.herokuapp.com/api/v1/searches?query=#{query}&field=#{field}")
+        # uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
         puts uri
         # http = Net::HTTP.new(uri.host, uri.port)
 
