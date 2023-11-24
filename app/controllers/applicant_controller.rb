230a536b04eb0e36b3e5da14198b34588a399b76
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-11-24 10:22:21 -0600

    dynamic db search review update

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -1,19 +1,8 @@
 class ApplicantController < ApplicationController
-    def index
-        # begin
-          @result = SearchService.search(query: "*application_cas_id=#{params[:cas_id]}", field: nil, token: session[:jwt_token])[0]
-      
-          if @result
-            @other_reviewers = Review.where(applicant_id: @result["application_cas_id"], status: "completed").where.not(user_netid: current_user.user_netid)
-          else
-            @other_reviewers = []
-            flash.now[:alert] = "No data found for the specified CAS ID."
-          end
-      
-        # rescue => e
-        #   Rails.logger.error "Failed to fetch data for CAS ID #{params[:cas_id]}: #{e.message}"
-        #   @other_reviewers = []
-        #   flash.now[:alert] = "There was a problem fetching application data. Please try again later."
-        # end
-      end    
+  def index
+    @result = SearchService.search(query: "*cas_id=#{params[:cas_id]}", field: "cas_id", token: session[:jwt_token])[0]
+    puts @result
+    # Retrieve reviews for the applicant
+    @other_reviewers = Review.where(applicant_id: @result["cas_id"], status: "completed").where.not(user_netid: current_user.user_netid)
+  end
 end
