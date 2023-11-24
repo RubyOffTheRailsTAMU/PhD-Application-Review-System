b512788b5dffa03ed224e10a6664e50813cc3374
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-11-24 15:53:50 -0600

    fix applicants_controller

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -1,8 +1,9 @@
 class ApplicantController < ApplicationController
   def index
     @result = SearchService.search(query: "*cas_id=#{params[:cas_id]}", field: "cas_id", token: session[:jwt_token])[0]
+    puts "result:"
     puts @result
     # Retrieve reviews for the applicant
-    @other_reviewers = Review.where(applicant_id: @result["cas_id"], status: "completed").where.not(user_netid: current_user.user_netid)
+    @other_reviewers = Review.where(applicant_id: params[:cas_id], status: "completed").where.not(user_netid: current_user.user_netid)
   end
 end
