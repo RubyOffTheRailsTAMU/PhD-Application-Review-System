a26884a744679a5199ca08f494ca6d92fac3d7c3
XRFXRF <Felixxrf@outlook.com>
2023-11-24 16:45:26 -0600

    tem changes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
 class SearchesController < ApplicationController
-    require 'net/http'
-    before_action :require_user
-    protect_from_forgery with: :null_session
-    def index
-      @phd_app_url = ENV["PHD_APP_REVIEW_URL"]
-      if params[:query].present?
-        @results = SearchService.search(query: params[:query], field: params[:field], token: session[:jwt_token])
-      else
-        @results = []
-      end
-    end
+  require 'net/http'
+  before_action :require_user
+  # protect_from_forgery with: :null_session
+  def index
+    @phd_app_url = ENV['PHD_APP_REVIEW_URL']
+    @results = if params[:query].present?
+                 SearchService.search(query: params[:query], field: params[:field], token: session[:jwt_token])
+               else
+                 []
+               end
   end
+end
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,34 +1,35 @@
 class SearchService
-    require 'net/http'
-    def self.searchall(token:)
-        uri = URI(ENV["DATABASE_SERVER_URL"]+"/api/v1/allapplicants")
-        headers = {
-            'Authorization' => "Bearer #{token}"
-        }
-        response = Net::HTTP.get_response(uri, headers)
-        JSON.parse(response.body)
-        # uri = URI("http://
-    end
-    def self.search(query:, field: ,token:)
-        uri = URI(ENV["DATABASE_SERVER_URL"]+"/api/v1/searches?query=#{query}&field=#{field}")
-        # uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
-        puts uri
-        # http = Net::HTTP.new(uri.host, uri.port)
+  require 'net/http'
+  def self.searchall(token:)
+    puts "token in service #{token}"
+    uri = URI(ENV['DATABASE_SERVER_URL'] + '/api/v1/allapplicants')
+    headers = {
+      'Authorization' => "Bearer #{token}"
+    }
+    response = Net::HTTP.get_response(uri, headers)
+    JSON.parse(response.body)
+    # uri = URI("http://
+  end
 
-        # request = Net::HTTP::Get.new(uri)
-        # request['Authorization'] = "Bearer #{token}"
+  def self.search(query:, field:, token:)
+    uri = URI(ENV['DATABASE_SERVER_URL'] + "/api/v1/searches?query=#{query}&field=#{field}")
+    # uri = URI("http://127.0.0.1:3001/api/v1/searches?query=#{query}&field=#{field}")
+    puts uri
+    # http = Net::HTTP.new(uri.host, uri.port)
 
-        # response = http.request(request)
-        # JSON.parse(response.body)
+    # request = Net::HTTP::Get.new(uri)
+    # request['Authorization'] = "Bearer #{token}"
 
+    # response = http.request(request)
+    # JSON.parse(response.body)
 
-        # response = Net::HTTP.get(uri)
-        headers = {
-            'Authorization' => "Bearer #{token}"
-          }
+    # response = Net::HTTP.get(uri)
+    headers = {
+      'Authorization' => "Bearer #{token}"
+    }
 
-        response = Net::HTTP.get_response(uri, headers)
-        puts response.body
-        JSON.parse(response.body)
-    end
+    response = Net::HTTP.get_response(uri, headers)
+    puts response.body
+    JSON.parse(response.body)
+  end
 end
