62a8c5c2fdd3f676e321649303d594973ab19a9b
tanaypatankar <tanaypatankar.2000@gmail.com>
2023-12-01 14:54:14 -0600

    BREAKING CHANGE: return token from google login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,38 +19,39 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
+    render json: {"token": request.env['omniauth.auth']}
 
-    emails = read_emails(Rails.root.join('app', 'assets', 'whitelist.txt').to_s)
-    if email_exists?(auth.info.email, emails)
-      session[:user_token] = auth.credentials.token
-      session[:user_email] = auth.info.email
-      # TODO: change this redirect to the user home page in the future
-      # TODO: create/get User with this email and use that user from this point onwards
+    # emails = read_emails(Rails.root.join('app', 'assets', 'whitelist.txt').to_s)
+    # if email_exists?(auth.info.email, emails)
+    #   session[:user_token] = auth.credentials.token
+    #   session[:user_email] = auth.info.email
+    #   # TODO: change this redirect to the user home page in the future
+    #   # TODO: create/get User with this email and use that user from this point onwards
 
-        jwtToken=JwtService.generate_jwt({user_email:session[:user_email],
-          exp: 24.hours.from_now.to_i})
-        session[:jwt_token] = jwtToken
+    #     jwtToken=JwtService.generate_jwt({user_email:session[:user_email],
+    #       exp: 24.hours.from_now.to_i})
+    #     session[:jwt_token] = jwtToken
         
 
-        user = User.find_by(user_email: session[:user_email])
-        if user
-          redirect_to '/home'
-        else
-          redirect_to '/users/new'
-        end
+    #     user = User.find_by(user_email: session[:user_email])
+    #     if user
+    #       redirect_to '/home'
+    #     else
+    #       redirect_to '/users/new'
+    #     end
        
-      else
-        session[:user_email] = auth.info.email
-        session[:alert] = 'This user is not allowed access to this site'
-        redirect_to root_path
-      end
-      # user = User.find_by(email: email)
-
-    # if user.nil?
-    #     redirect_to new_user_path, alert: 'Create an account before signing in with Google'
-    # else
+    #   else
+    #     session[:user_email] = auth.info.email
+    #     session[:alert] = 'This user is not allowed access to this site'
     #     redirect_to root_path
-    # end
+    #   end
+    #   # user = User.find_by(email: email)
+
+    # # if user.nil?
+    # #     redirect_to new_user_path, alert: 'Create an account before signing in with Google'
+    # # else
+    # #     redirect_to root_path
+    # # end
   end
 
   def destroy
